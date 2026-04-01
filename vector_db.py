f19c4b064f284665b0310278628837435365e3f6
efe5214mobil-svg <efe5214mobil@gmail.com>
2026-04-01 21:36:32 +0300

    Update vector_db.py

--- vector_db.py
+++ vector_db.py
@@ -1,1 +1,12 @@
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
+def load_vector_db():
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    db = Chroma(
+        persist_directory="okul_asistani_gpt_db",
+        embedding_function=embeddings
+    )
+
+    return db
